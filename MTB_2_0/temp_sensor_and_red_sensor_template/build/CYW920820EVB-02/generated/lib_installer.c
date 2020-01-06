dc0546a231664103eb01ee66a403cb58bbbad00d
MFRoberts <41492728+MFRoberts@users.noreply.github.com>
2020-01-06 10:00:25 -0800

    Update temp sensor and red sensor project and template

---
+++ MTB_2_0/temp_sensor_and_red_sensor_template/build/CYW920820EVB-02/generated/lib_installer.c
@@ -0,0 +1,16 @@
+// ./../wiced_btsdk/dev-kit/baselib/20820A1/make/scripts/wiced-gen-lib-installer.pl
+// args: 
+// output: C:/Users/A62584/mtw2_0/sensor_temperature_and_red_sensor/build/CYW920820EVB-02/generated/lib_installer.c
+#include <stdint.h>
+typedef struct tag_PATCH_TABLE_ENTRY_t {
+	uint32_t breakout;
+	uint32_t replacement;
+} PATCH_TABLE_ENTRY_t;
+void patch_autoInstall(uint32_t old_address, uint32_t new_address);
+void patch_autoReplace(uint32_t breakout_address, uint32_t replacement);
+void patch_autoReplaceData(uint32_t breakout_address, uint32_t replacement);
+void install_libs(void);
+
+void install_libs(void)
+{
+}
